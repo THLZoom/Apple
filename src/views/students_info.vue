ad6f8116b99a5fd4782644959645ec54feae639e
jame0205 <gaozhan@dxwt.com>
2018-04-16 16:58:18 +0800

    布局ing
    布局ing

--- src/views/students_info.vue
+++ src/views/students_info.vue
@@ -1,39 +1,9 @@
 <template>
   <div class="students_info">
-    <el-table v-loading="loading" :data="tableData" ref="tableData" :border="true" style="width: 100%;" size="mini" @select="getSelectData" @select-all="clearSelect">
-        <el-table-column type="selection" width="55" :resizable="false" align="center" fixed="left"></el-table-column>
-        <el-table-column prop="name" label="姓名" min-width="120" align="left" show-overflow-tooltip></el-table-column>
-        <el-table-column prop="giveresult" label="领取状态" min-width="100" align="center">
-          <template slot-scope="scope">
-            <el-tag type="danger" v-if="scope.row.giveresult == 0" size="medium">未领取</el-tag>
-            <el-tag v-else-if="scope.row.giveresult == 1" size="medium">已领取</el-tag>
-            <el-tag v-else-if="scope.row.giveresult == 2" size="medium">已领取</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="giveresult" label="充值状态" min-width="100" align="center">
-          <template slot-scope="scope">
-            <el-tag type="danger" v-if="scope.row.giveresult == 1" size="medium">未充值</el-tag>
-            <el-tag v-if="scope.row.giveresult == 2" size="medium">已充值</el-tag>
-            <el-tag v-if="scope.row.giveresult == 3" size="medium">充值失败</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="errmsg" label="失败原因" min-width="100" align="left" show-overflow-tooltip></el-table-column>
-        <el-table-column prop="subscribe" label="关注状态" min-width="100" align="center">
-          <template slot-scope="scope">
-            <el-tag v-if="scope.row.subscribe == 1" size="medium">已关注</el-tag>
-            <el-tag type="danger" v-else-if="scope.row.subscribe == 0" size="medium">未关注</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="status" label="绑定状态" min-width="100" align="center">
-          <template slot-scope="scope">
-            <el-tag v-if="scope.row.status == 1" size="medium">已绑定</el-tag>
-            <el-tag type="danger" v-else-if="scope.row.status == 2" size="medium">未绑定</el-tag>
-          </template>
-        </el-table-column>
-        <el-table-column prop="custphone" label="绑定号码" min-width="100" align="left"></el-table-column>
-        <el-table-column prop="fist_bind_time" label="首绑时间" min-width="140" align="left"></el-table-column>
-        <el-table-column prop="givegifttime" label="领取时间" min-width="140" align="left"></el-table-column>
-      </el-table>
+    <el-table v-loading="loading" :data="tableData" ref="tableData" :border="true" style="width: 100%;" size="mini">
+      
+    </el-table>
+    <div class="tools"></div>
   </div>
 </template>
 <script>
@@ -41,6 +11,8 @@ export default {
   name: 'students_info',
   data(){
     return {
+      tableData: [],
+      loading: false,
     }
   },
 }
@@ -48,6 +20,16 @@ export default {
 <style scoped>
   .students_info{
     height: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  .tools{
+    width: 100%;
+    height: 50px;
+    background: #f2f2f2;
+    padding: 10px 10px 0;
+    margin: 10px 0;
+    align-self: flex-end;
   }
 </style>
 
